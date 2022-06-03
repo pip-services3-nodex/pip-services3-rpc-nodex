a7dca85c02aeb779b493b00385ed1dd251ff367f
Dmitry Krayniy <krdima92@gmail.com>
2022-06-03 15:29:15 +0400

    Updated release script

--- release.ps1
+++ release.ps1
@@ -9,23 +9,30 @@ $package = Get-Content -Path "package.json" | ConvertFrom-Json
 
 # Verify component and package version
 if ($component.version -ne $package.version) {
-    throw "Versions in component.json and package.json do not match"
+    Write-Error "Versions in component.json and package.json do not match"
 }
 
-# Automatically login to npm registry
-if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null -and $env:NPM_EMAIL -ne $null) {
+# Automatically login to npm registry by npm-cli-login
+if (-not [string]::IsNullOrEmpty($env:NPM_USER) -and`
+    -not [string]::IsNullOrEmpty($env:NPM_PASS) -and`
+    -not [string]::IsNullOrEmpty($env:NPM_EMAIL)) {
     if (npm whoami -ne $env:NPM_USER) {
-        Write-Host "Logging to npmjs registry..."
+        Write-Host "Logging to npmjs registry by npm-cli-login..."
         npm-cli-login
     }
 } 
 
+# Automatically login to npm registry by putting token to .npmrc
+if (-not [string]::IsNullOrEmpty($env:NPM_TOKEN)) {
+    Write-Host "Creating project scope .npmrc"
+    Set-Content -Path ".npmrc" -Value '//registry.npmjs.org/:_authToken=${NPM_TOKEN}'
+}
+
 Write-Host "Getting $($package.name) versions from npm registry..."
 
 # Check if version exist on npmjs
-$npmjsPackageVersions = npm view $package.name versions
-
-if ($npmjsPackageVersions -ne $null -and $npmjsPackageVersions.Contains($package.version)) {
+$npmjsPackageVersions = npm view $package.name versions | ConvertFrom-Json
+if ($npmjsPackageVersions -contains $package.version) {
     Write-Host "Package already exists on npmjs, publish skipped."
 } else {
     # Publish to npm repository
