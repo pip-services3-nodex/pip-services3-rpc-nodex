6d2ec76bba0ce5d825d35f15b0a46eb2e1706d07
Dmitry Krayniy <krdima92@gmail.com>
2022-08-16 20:43:54 +0400

    Updated pipeline

--- build.ps1
+++ build.ps1
@@ -3,32 +3,53 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-
-# Get buildnumber from github actions
-if ($env:GITHUB_RUN_NUMBER -ne $null) {
-    $component.build = $env:GITHUB_RUN_NUMBER
-    Set-Content -Path "component.json" -Value $($component | ConvertTo-Json)
-}
-
-$buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$buildImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
 $container=$component.name
 
 # Remove build files
-if (Test-Path "obj") {
-    Remove-Item -Recurse -Force -Path "obj"
+if (Test-Path -Path "$PSScriptRoot/obj") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/obj"
+}
+
+# Copy private keys to access git repo
+if (-not (Test-Path -Path "$PSScriptRoot/docker/id_rsa")) {
+    if (-not [string]::IsNullOrEmpty($env:GIT_PRIVATE_KEY)) {
+        Write-Host "Creating docker/id_rsa from environment variable..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value $env:GIT_PRIVATE_KEY
+    } elseif (Test-Path -Path "~/.ssh/id_rsa") {
+        Write-Host "Copying ~/.ssh/id_rsa to docker..."
+        Copy-Item -Path "~/.ssh/id_rsa" -Destination "docker"
+    } else {
+        Write-Host "Missing ~/.ssh/id_rsa file..."
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value ""
+    }
+}
+
+# Copy .npmrc to docker folder to use it inside container
+if (-not (Test-Path -Path "$PSScriptRoot/docker/.npmrc")) {
+    if (-not [string]::IsNullOrEmpty($env:NPM_TOKEN)) {
+        Write-Host "Creating docker/.npmrc from environment variable..."
+        Set-Content -Path "$PSScriptRoot/docker/.npmrc" -Value "//registry.npmjs.org/:_authToken=$($env:NPM_TOKEN)"
+    } elseif (Test-Path -Path "~/.npmrc") {
+        Write-Host "Copying ~/.npmrc to docker..."
+        Copy-Item -Path "~/.npmrc" -Destination "docker" 
+    } else {
+        Write-Host "Missing ~/.npmrc file..."
+        Set-Content -Path "$PSScriptRoot/docker/.npmrc" -Value ""
+    }
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.build -t $buildImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.build" -t $buildImage .
 
 # Create and copy compiled files, then destroy
 docker create --name $container $buildImage
-docker cp "$($container):/app/obj" ./obj
+docker cp "$($container):/app/obj" "$PSScriptRoot/obj"
 docker rm $container
 
-if (!(Test-Path ./obj)) {
-    Write-Host "obj folder doesn't exist in root dir. Build failed. Watch logs above."
-    exit 1
+# Verify that obj folder was indeed created after build
+if (-not (Test-Path -Path "$PSScriptRoot/obj")) {
+    Write-Error "obj folder doesn't exist in root dir. Build failed. See logs above for more information."
 }
--- docgen.ps1
+++ docgen.ps1
@@ -3,21 +3,25 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-
-$docsImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$docsImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
 $container=$component.name
 
 # Remove documentation files
-if (Test-Path "docs") {
-    Remove-Item -Recurse -Force -Path "docs"
+if (Test-Path -Path "$PSScriptRoot/docs") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/docs"
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.docs -t $docsImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.docs" -t $docsImage .
 
 # Create and copy compiled files, then destroy
 docker create --name $container $docsImage
-docker cp "$($container):/app/docs" ./docs
+docker cp "$($container):/app/docs" "$PSScriptRoot/docs"
 docker rm $container
+
+# Verify that docs folder was indeed created after generating documentation
+if (-not (Test-Path "$PSScriptRoot/docs")) {
+    Write-Error "docs folder doesn't exist in root dir. Build failed. See logs above for more information."
+}
